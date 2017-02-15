6e0ad0677a22d05172e40dd9a12182d0300ddc96
4shadoww <4shadoww0@gmail.com>
2017-02-15 18:49:32 +0200

    commit

--- core/log.py
+++ core/log.py
@@ -18,8 +18,8 @@ def printlog(*message, end='\n'):
 	time = datetime.datetime.now()
 	line = str(time)+' '+finalmessage+end
 	if config.enable_log == True:
-		logfile.write(line.encode('utf-8'))
-	sys.stdout.write(line.encode('utf-8'))
+		logfile.write(line)
+	sys.stdout.write(line)
 
 def log(*message, end='\n'):
 	finalmessage = ""
@@ -31,7 +31,7 @@ def log(*message, end='\n'):
 	if config.enable_log == True:
 		time = datetime.datetime.now()
 		line = str(time)+' '+finalmessage+end
-		logfile.write(line.encode('utf-8'))
+		logfile.write(line)
 
 warnings = []
 
