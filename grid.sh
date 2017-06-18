865d9cce0d6d3df8bf7d6860022dd79373f9e43e
4shadoww <4shadoww0@gmail.com>
2017-06-18 19:54:17 +0300

    commit

--- grid.sh
+++ grid.sh
@@ -6,7 +6,7 @@ export TZ="/usr/share/zoneinfo/Europe/Helsinki"
 # Set locale
 export LANGUAGE="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
-export PYTHONIOENCODING=UTF-8
+export PYTHONIOENCODING="UTF-8"
 
 # Start thanatos
 THANATOS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/thanatos.py"
