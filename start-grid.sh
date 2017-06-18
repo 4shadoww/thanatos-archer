7839ea2ef17042fcafb8c761d02b9337f1c9a12f
4shadoww <4shadoww0@gmail.com>
2017-06-18 19:53:49 +0300

    commit

--- start-grid.sh
+++ start-grid.sh
@@ -1,9 +1,3 @@
-# Set time zone
-export TZ="/usr/share/zoneinfo/Europe/Helsinki"
-
-# Set locale
-export LANGUAGE="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export PYTHONIOENCODING=UTF-8
-
-jsub -sync y -mem 200m -N thanatos thanatos.py
+#!/bin/bash
+GRID="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/grid.sh"
+jsub -sync y -mem 200m -N $GRID
