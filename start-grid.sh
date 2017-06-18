1ebc467ba91727b342ee1115756c8616875f459c
4shadoww <4shadoww0@gmail.com>
2017-06-18 19:39:25 +0300

    commit

--- start-grid.sh
+++ start-grid.sh
@@ -1,1 +1,9 @@
+# Set time zone
+export TZ="/usr/share/zoneinfo/Europe/Helsinki"
+
+# Set locale
+export LANGUAGE="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export PYTHONIOENCODING=UTF-8
+
 jsub -mem 200m -N thanatos thanatos.py
